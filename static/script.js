48f7600573de1a67bf1ea820659827a13f7bb258
Sagar Saxena <ssaxena1@umd.edu>
2020-09-27 04:10:06 -0400

    Add Sliders and Generate Request

--- static/script.js
+++ static/script.js
@@ -1,7 +1,19 @@
 let map;
 let circles = [];
+let cent = { lat: 37.5483, lng: -77.4527};
+let zip;
+let radii = [];
 
-function setRings(center, zoom, rings) {
+function build_request(){
+  var geocoder = new google.maps.Geocoder;
+  geocoder.geocode({'location': cent}, function(results, status) {
+    console.log({"center": cent, "zip": zip, "radii": radii})
+    var zip = results[0]["address_components"].filter(function(c){ return c["types"] == "postal_code"})[0].short_name
+    return {"center": cent, "zip": zip, "radii": radii};
+  });
+}
+
+function setRings(center, zoom, rings, miles) {
 	map.setZoom(zoom)
 	for (i=0; i < rings; i++) {
 		var cityCircle = new google.maps.Circle({
@@ -12,12 +24,14 @@ function setRings(center, zoom, rings) {
             fillOpacity: 0,
             map,
             center: center,
-            radius: 160934*i,
+            radius: 1609.34*miles*i,
           });
 		circles.push(cityCircle)
+        radii.push(1609.34*miles*i)
 	}
 	return map;
 }
+
 function remove_circle(circle) {
     // remove event listers
 	console.log(circle)
@@ -34,16 +48,22 @@ function clearRings() {
 	var l = circles.length
 	for (i=0; i < l; i++) { 
 		remove_circle(circles.pop())
+        radii.pop();
 	}
 }
 function initMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
-		center: { lat: 37.5483, lng: -77.4527},
+		center: cent,
 		zoom: 5,
 	});
-	setRings({lat: 37.5483, lng: -77.4527}, 5, 30)
+	setRings(cent, 5, 30, 100)
+
     const input = document.getElementById("search");
 	const autocomplete = new google.maps.places.Autocomplete(input);
+	const rings = document.getElementById("rings");
+	const miles = document.getElementById("miles");
+    const geocoder = new google.maps.Geocoder;
+
 	autocomplete.addListener("place_changed", () => {
 		const place = autocomplete.getPlace();
 		if (!place.geometry) {
@@ -52,12 +72,23 @@ function initMap() {
       		window.alert("No details available for input: '" + place.name + "'");
       		return;
     	}
-		
+	    cent = place.geometry.location;
 		map.setCenter(place.geometry.location);
 		clearRings();
-		setRings(place.geometry.location, 5, 30);
+		setRings(place.geometry.location, 5, rings.value, miles.value);
 	});
-		
+	
+	rings.addEventListener("change", function() {
+      console.log(rings.value);
+	  clearRings();
+	  setRings(cent, 5, rings.value, miles.value);
+    });
+	
+    miles.addEventListener("change", function() {
+      console.log(rings.value);
+	  clearRings();
+	  setRings(cent, 5, rings.value, miles.value);
+    });
 }
 
 function init() {
